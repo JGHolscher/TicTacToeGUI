24ffb5e73993ebc1c8c3f3b0ff34bea85cba4bca
JGHolscher <holschjg@mail.uc.edu>
2022-09-27 10:46:16 -0400

    initial

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -1,4 +1,11 @@
-public class TicTacToeFrame
+import java.util.Scanner;
+import javax.swing.*;
+
+public class TicTacToeFrame extends JFrame
 {
+    JPanel mainPnl, titlePnl, btnPnl;
+
+    JButton 11,12,13,21,22,23,31,32,33;
+
 
 }
