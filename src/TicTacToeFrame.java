bccf4e2c17b9fe02bd26e4767034eaea3e4925ec
JGHolscher <holschjg@mail.uc.edu>
2022-09-27 15:21:06 -0400

    initial

--- src/TicTacToeFrame.java
+++ src/TicTacToeFrame.java
@@ -16,10 +16,11 @@ public class TicTacToeFrame extends JFrame
 
     String player = "X";
     JPanel mainPnl, titlePnl, btnPnl, quitPnl;
+    JOptionPane pane;
 
-    int ROW = 3;
-    int COL = 3;
-    JButton[][] board = new JButton[3][3];
+    static int ROW = 3;
+    static int COL = 3;
+    static JButton[][] board = new JButton[3][3];
 
     JButton quitBtn;
 
@@ -80,7 +81,7 @@ public class TicTacToeFrame extends JFrame
                         {player = "O";}
                         else {player = "X";}
 
-                       // displayWinner();
+                       // displayResult();
                     }
                 });
                 btnPnl.add(board[row][col]);
@@ -88,13 +89,186 @@ public class TicTacToeFrame extends JFrame
         }
     }
 
+public void displayWin()
+    {
+        if(isWin(player) == true)
+        {
+            int windowResult = JOptionPane.showConfirmDialog(pane, "Game Over. " + player + "wins! + Would you like to play again?", "f", JOptionPane.YES_NO_OPTION);
+
+            if (windowResult == JOptionPane.YES_OPTION) {
+                clearBoard();
+            }
+
+            if (windowResult == JOptionPane.NO_OPTION) {
+                System.exit(0);
+            }
+        }
+        else if(isTie())
 
 
+    }
+    private static void clearBoard()
+    {
+        // sets all the board elements to a space
+        for( int row = 0; row < 3; row++)
+        {
+            for (int col = 0; col < 3; col++)
+            {
+                board[row][col].setText(" ");
+            }
+        }
+    }
+    private static boolean isWin(String player)
+    {
+        if(isColWin(player) || isRowWin(player) || isDiagnalWin(player))
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private static boolean isColWin(String player)
+    {
+        // checks for a col win for specified player
+        for(int col=0; col < COL; col++)
+        {
+            if(board[0][col].equals(player) &&
+                    board[1][col].equals(player) &&
+                    board[2][col].equals(player))
+            {
+                return true;
+            }
+        }
+        return false; // no col win
+    }
+
+    private static boolean isRowWin(String player)
+    {
+        // checks for a row win for the specified player
+        for(int row=0; row < ROW; row++)
+        {
+            if(board[row][0].equals(player) &&
+                    board[row][1].equals(player) &&
+                    board[row][2].equals(player))
+            {
+                return true;
+            }
+        }
+        return false; // no row win
+    }
 
 
+    private static boolean isDiagnalWin(String player)
+    {
+        // checks for a diagonal win for the specified player
 
+        if(board[0][0].equals(player) &&
+                board[1][1].equals(player) &&
+                board[2][2].equals(player) )
+        {
+            return true;
+        }
+        if(board[0][2].equals(player) &&
+                board[1][1].equals(player) &&
+                board[2][0].equals(player) )
+        {
+            return true;
+        }
+        return false;
+    }
 
+    private static boolean isTie()
+    {
+        boolean xFlag = false;
+        boolean oFlag = false;
+        // Check all 8 win vectors for an X and O so
+        // no win is possible
+        // Check for row ties
+        for(int row=0; row < ROW; row++)
+        {
+            if(board[row][0].equals("X") ||
+                    board[row][1].equals("X") ||
+                    board[row][2].equals("X"))
+            {
+                xFlag = true; // there is an X in this row
+            }
+            if(board[row][0].equals("O") ||
+                    board[row][1].equals("O") ||
+                    board[row][2].equals("O"))
+            {
+                oFlag = true; // there is an O in this row
+            }
 
+            if(! (xFlag && oFlag) )
+            {
+                return false; // No tie can still have a row win
+            }
+
+            xFlag = oFlag = false;
+
+        }
+        // Now scan the columns
+        for(int col=0; col < COL; col++)
+        {
+            if(board[0][col].equals("X") ||
+                    board[1][col].equals("X") ||
+                    board[2][col].equals("X"))
+            {
+                xFlag = true; // there is an X in this col
+            }
+            if(board[0][col].equals("O") ||
+                    board[1][col].equals("O") ||
+                    board[2][col].equals("O"))
+            {
+                oFlag = true; // there is an O in this col
+            }
+
+            if(! (xFlag && oFlag) )
+            {
+                return false; // No tie can still have a col win
+            }
+        }
+        // Now check for the diagonals
+        xFlag = oFlag = false;
+
+        if(board[0][0].equals("X") ||
+                board[1][1].equals("X") ||
+                board[2][2].equals("X") )
+        {
+            xFlag = true;
+        }
+        if(board[0][0].equals("O") ||
+                board[1][1].equals("O") ||
+                board[2][2].equals("O") )
+        {
+            oFlag = true;
+        }
+        if(! (xFlag && oFlag) )
+        {
+            return false; // No tie can still have a diag win
+        }
+        xFlag = oFlag = false;
+
+        if(board[0][2].equals("X") ||
+                board[1][1].equals("X") ||
+                board[2][0].equals("X") )
+        {
+            xFlag =  true;
+        }
+        if(board[0][2].equals("O") ||
+                board[1][1].equals("O") ||
+                board[2][0].equals("O") )
+        {
+            oFlag =  true;
+        }
+        if(! (xFlag && oFlag) )
+        {
+            return false; // No tie can still have a diag win
+        }
+
+        // Checked every vector so I know I have a tie
+        return true;
+    }
 
 
 
